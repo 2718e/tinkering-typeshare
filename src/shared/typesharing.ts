e5dbb816abc344fce44c91daa37529eda7b0631b
Dan <livelyandexciting@gmail.com>
2018-06-24 20:56:10 +0100

    started attempt to do type sharing with mapped types

--- src/shared/typesharing.ts
+++ src/shared/typesharing.ts
@@ -1,21 +1,35 @@
+import { Router } from 'express-serve-static-core';
 
-interface IServerFunctionContract<TQueryParams, TBodyParams, TReturn> {
-  url: string,
-  methodType: string
+type RestMethod = "get" | "post" | "put" | "delete";
+
+interface ServerMethod<TQueryParams, TBodyParams, TReturn> {
+
+  restMethod: RestMethod
+  computeValue: (params: TBodyParams & TQueryParams) => TReturn
+  "queryType"?: TQueryParams
+  "bodyType"?: TBodyParams // should never assign these properties but having them allows to extract the precise type of the generic when mapping the type.
+  "retType"?: TReturn
+
 }
 
-function parseParams(req) {
-
+interface IOrder {
+  quantity: number
+  description: string
 }
 
-function FulfilContract<TQueryParams, TBodyParams, TReturn>(
-  expressRouter: any,
-  contract: IServerFunctionContract<TQueryParams, TBodyParams, TReturn>,
-  fComputeReturn: (query: TQueryParams, body: TBodyParams) => TReturn) {
-  expressRouter[contract.methodType](contract.url, async (req, res) => {
-        const { queryParams, bodyParams } = parseParams(req)
-        const result = fComputeReturn(queryParams,bodyParams);
-        res.send(result);
-    })
+type SMDict = {[s:string] : ServerMethod<any,any,any>}
 
+// developer should fill this in 
+interface IServerApi extends SMDict{
+  "getOrderById": ServerMethod<string, void, IOrder>
+}
+
+type ServerMethodCaller<TM extends ServerMethod<any,any,any>> = (qp: TM["queryType"], bp:TM["bodyType"] ) => Promise<TM["retType"]>
+
+type ClientApi<TServerApi extends SMDict > = { 
+  [m in keyof TServerApi]: ServerMethodCaller<TServerApi[m]>
+}
+
+const ClientCaller: ClientApi<IServerApi> = {
+  "getOrderById": async (id: string) => { return {quantity:7, description: id}}
 }
